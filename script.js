3a6094b3197cd2e1d2a79bc1c3c4bfd077fb1aa6
Gabi <gabrielleonardomendoza19@gmail.com>
2021-08-08 20:35:27 -0300

    Primer commit resolviendo problemas

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. 
+//     miFuncion("Hola Mundo") devolverá 10.
+// 2) Programa una función que te devuelva el texto recortado según el número de caracteres 
+//     indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
+// 3) Programa una función que dada una String te devuelva un Array de textos separados por 
+//     cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+// 4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá
+//     Hola Mundo Hola Mundo Hola Mundo.
+
+// 1
+function numeroDeContenido(a){
+
+    if(!isNaN(a)){
+        console.log("Acabas de mandar numeros");
+    }else if(a == undefined){        
+        console.log("No escribiste nada");
+    }else{
+        let respuesta = a.length;
+        console.log(respuesta);
+    }
+}
+
+numeroDeContenido();
+
+// 2
+function extraer(texto, numero){
+    let ejemplo = texto;
+    console.log(ejemplo.substring(0, numero));
+}
+
+extraer("Hola mundo", 7);
+
+// cadena.substring(indiceA[, indiceB'])
